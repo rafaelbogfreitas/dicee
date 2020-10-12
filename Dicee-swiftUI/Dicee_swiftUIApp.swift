f18def7c95ea6fd1271c9b14a5730e2fc12e9fc0
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-10-12 11:53:09 -0300

    First commit

---
+++ Dicee-swiftUI/Dicee_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_swiftUIApp.swift
+//  Dicee-swiftUI
+//
+//  Created by Rafael Freitas on 12/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
